439b6c87e208c91f0424c0ead150d9a13a3b5f93
Wellington Ferro <w.ferro2@gmail.com>
2018-02-15 21:04:50 +0000

    Adding About, Jokes and Home pages along with menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+  get 'about/index'
+  resources :jokes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
